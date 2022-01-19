2c492896a8d2030431fd5ee1fa7ec16e16b64821
Jackson Rolando <rolandoj@msoe.edu>
2022-01-18 22:56:44 -0600

    added arrowheads, woo!

--- src/networks/Line.java
+++ src/networks/Line.java
@@ -5,6 +5,7 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 
 public class Line {
+    private final double ARROW_LENGTH = 5;
     private Node start;
     private Node end;
 
@@ -26,6 +27,12 @@ public class Line {
         Point2D lineEnd = p2.subtract(endRad);
 
         context.strokeLine(lineStart.getX(), lineStart.getY(), lineEnd.getX(), lineEnd.getY());
+
+        Point2D arrowEnd1 = new Point2D((lineEnd.getX() - 7 * direction.getX()) + 4 * direction.getY(), (lineEnd.getY() - 7 * direction.getY()) + 4 * -1 * direction.getX());
+        Point2D arrowEnd2 = new Point2D((lineEnd.getX() - 7 * direction.getX()) - 4 * direction.getY(), (lineEnd.getY() - 7 * direction.getY()) - 4 * -1 * direction.getX());
+
+        context.strokeLine(arrowEnd1.getX(), arrowEnd1.getY(), lineEnd.getX(), lineEnd.getY());
+        context.strokeLine(arrowEnd2.getX(), arrowEnd2.getY(), lineEnd.getX(), lineEnd.getY());
     }
 
     public Node getStart() {
